e46dc93697d82cd1d0114adb271febbd7780451d
林乃胜 <876056609@qq.com>
2017-04-16 11:32:58 +0800

    nothing but child's play

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Trend
 
-TODO  
+TODO  list
 1. 爬虫  
-2. Elasticsearch  
+2. ~~Elasticsearch~~  
 3. ~~flyway~~
+4. redis 
+5. 再搞点什么？
--- facade_of_stream/src/main/java/xyz/liinns/Application.java
+++ facade_of_stream/src/main/java/xyz/liinns/Application.java
@@ -6,6 +6,10 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Properties;
+
 /**
  * Description:
  * Created by LiinNs on 2017-4-10 0010.
@@ -14,56 +18,26 @@ import org.springframework.context.annotation.Bean;
 public class Application /*implements CommandLineRunner */{
 
     public static void main(String[] args) {
+        System.setProperty("user.timezone", "Asia/Shanghai");
         new SpringApplicationBuilder(Application.class).web(true).run(args);
     }
 
     @Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
-
-            System.out.println("Let's inspect the beans provided by Spring Boot:");
-
+            System.out.println("------- Let's inspect the beans provided by Spring Boot -------");
             String[] beanNames = ctx.getBeanDefinitionNames();
-            /*Arrays.sort(beanNames);*/
+            Arrays.sort(beanNames);
             for (String beanName : beanNames) {
                 System.out.println(beanName);
             }
 
+            System.out.println("---------------------------- properties ----------------------------");
+            Properties properties = System.getProperties();
+            for (Map.Entry entry : properties.entrySet()) {
+                System.out.println(String.format("%30s", entry.getKey().toString()) + "===:===" + entry.getValue());
+            }
         };
     }
-
-    /*@Override
-    public void run(String... args) throws Exception {
-        this.repository.deleteAll();
-        saveCustomers();
-        fetchAllCustomers();
-        fetchIndividualCustomers();
-    }
-
-    private void saveCustomers() {
-        this.repository.save(new Customer("Alice", "Smith"));
-        this.repository.save(new Customer("Bob", "Smith"));
-    }
-
-    private void fetchAllCustomers() {
-        System.out.println("Customers found with findAll():");
-        System.out.println("-------------------------------");
-        for (Customer customer : this.repository.findAll()) {
-            System.out.println(customer);
-        }
-        System.out.println();
-    }
-
-    private void fetchIndividualCustomers() {
-        System.out.println("Customer found with findByFirstName('Alice'):");
-        System.out.println("--------------------------------");
-        System.out.println(this.repository.findByFirstName("Alice"));
-
-        System.out.println("Customers found with findByLastName('Smith'):");
-        System.out.println("--------------------------------");
-        for (Customer customer : this.repository.findByLastName("Smith")) {
-            System.out.println(customer);
-        }
-    }*/
 
 }
